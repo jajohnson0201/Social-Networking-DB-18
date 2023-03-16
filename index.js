efe4d21ea8332f7d9a8080676242c1d441ac6c40
joe <jajohnson0201@gmail.com>
2023-03-15 21:45:27 -0500

    got connection and index ready for testing on insomnia

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const express = require('express');
-const db = require('./config/connect');
 const routes = require('./routes');
-const PORT = process.env.PORT || 3001;
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
